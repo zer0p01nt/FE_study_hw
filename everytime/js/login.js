fcf0aa7f89f7a6a9401942c17543ac20414b76a4
zer0p01nt <1027.bmy@gmail.com>
2025-05-24 23:51:44 +0900

    [0524] 스터디 피드백 : 로그인 페이지

--- everytime/js/login.js
+++ everytime/js/login.js
@@ -1,6 +1,8 @@
 const id = document.getElementById("id");
 const pass = document.getElementById("password");
 const login = document.getElementById("login");
+const passBtn = document.querySelector(".pass-btn");
+const passBtnIcon = document.querySelector(".pass-btn-i");
 
 function checkInputs() {
   const idInput = id.value.trim();
@@ -13,5 +15,21 @@ function checkInputs() {
   }
 }
 
+function togglePassword() {
+  if (passBtn.classList.contains("active")) {
+    passBtn.classList.remove("active");
+    passBtnIcon.classList.replace("fa-eye-slash", "fa-eye");
+    pass.type = "password";
+  } else {
+    passBtn.classList.add("active");
+    passBtnIcon.classList.replace("fa-eye", "fa-eye-slash");
+    pass.type = "text";
+  }
+}
+
 id.addEventListener("input", checkInputs);
 pass.addEventListener("input", checkInputs);
+passBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  togglePassword();
+});
